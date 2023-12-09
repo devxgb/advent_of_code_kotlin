61ba2e299fb77a26eb05cb9aaf1729cecf510e13
G-Basak <basak.gunjan@gmail.com>
2023-12-09 19:31:07 +0530

    2023 day6

---
+++ src/main/kotlin/org/example/aoc/year2023/day6/Day6.kt
@@ -0,0 +1,55 @@
+package org.example.aoc.year2023.day6
+
+import org.example.common.readFile
+
+fun main() {
+    val lines = readFile("\\year2023\\day6\\input.txt")
+    val day6 = Day6()
+    println(day6.part1(lines))
+    println(day6.part2(lines))
+}
+
+class Day6 {
+    fun part1(lines: List<String>): Int {
+        return parsePart1(lines)
+            .map { (raceTime, recordDistance) ->
+                calculateNumberOfWin(raceTime.toLong(), recordDistance.toLong())
+            }
+            .reduce(Int::times)
+    }
+
+    fun part2(lines: List<String>): Int {
+        val (raceTime, recordDistance) = parsePart2(lines)
+        return calculateNumberOfWin(raceTime, recordDistance)
+    }
+
+    fun parsePart1(lines: List<String>): List<Pair<Int, Int>> {
+        require(lines.size == 2)
+        val raceTimes = lines[0].split(":")[1].trim().split(" +".toRegex()).map { it.toInt() }
+        val recordDistances = lines[1].split(":")[1].trim().split(" +".toRegex()).map { it.toInt() }
+        require(raceTimes.size == recordDistances.size)
+        return IntRange(0, raceTimes.size-1)
+            .map { raceTimes[it] to recordDistances[it] }
+    }
+
+    fun parsePart2(lines: List<String>): Pair<Long, Long> {
+        require(lines.size == 2)
+        val time = lines[0].split(":")[1].trim().replace(" ", "").toLong()
+        val recordDistance = lines[1].split(":")[1].trim().replace(" ", "").toLong()
+        return time to recordDistance
+    }
+
+    fun calculateDistanceTravelled(raceTime: Long, buttonPressTime: Long): Long {
+        require(buttonPressTime in 1..<raceTime)
+        val speed = buttonPressTime
+        val timeToTravel = raceTime - buttonPressTime
+        return speed * timeToTravel
+    }
+
+    fun calculateNumberOfWin(raceTime: Long, recordDistance: Long): Int {
+        return LongRange(1, raceTime - 1L)
+            .map { buttonPressTime -> calculateDistanceTravelled(raceTime, buttonPressTime) }
+            .count { distance -> distance > recordDistance }
+    }
+
+}
---
+++ src/test/kotlin/org/example/aoc/year2023/day6/Day6Test.kt
@@ -0,0 +1,58 @@
+package org.example.aoc.year2023.day6
+
+import org.example.common.readTestFile
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day6Test {
+
+    private val lines = readTestFile("\\year2023\\day6\\test_input.txt")
+    private val day6 = Day6()
+
+    @Test
+    fun part1() {
+        assertEquals(288, day6.part1(lines))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(71503, day6.part2(lines))
+    }
+
+    @Test
+    fun parsePart1Test() {
+        assertEquals(
+            listOf(
+                7 to 9,
+                15 to 40,
+                30 to 200
+            ),
+            day6.parsePart1(lines)
+        )
+    }
+
+    @Test
+    fun parsePart2Test() {
+        assertEquals(
+            71530L to 940200L, day6.parsePart2(lines)
+        )
+    }
+
+    @Test
+    fun calculateDistanceTravelledTest() {
+        assertEquals(6, day6.calculateDistanceTravelled(7, 1))
+        assertEquals(10, day6.calculateDistanceTravelled(7, 2))
+        assertEquals(12, day6.calculateDistanceTravelled(7, 3))
+        assertEquals(12, day6.calculateDistanceTravelled(7, 4))
+        assertEquals(10, day6.calculateDistanceTravelled(7, 5))
+        assertEquals(6, day6.calculateDistanceTravelled(7, 6))
+    }
+
+    @Test
+    fun calculateNumberOfWin() {
+        assertEquals(4, day6.calculateNumberOfWin(7, 9))
+        assertEquals(8, day6.calculateNumberOfWin(15, 40))
+        assertEquals(9, day6.calculateNumberOfWin(30, 200))
+        assertEquals(71503, day6.calculateNumberOfWin(71530, 940200))
+    }
+}
