1ed8d0da0c75125d1cbaee86daa0d6d177588dae
G-Basak <basak.gunjan@gmail.com>
2024-12-02 03:05:33 +0530

    2024 Day 1

---
+++ src/main/kotlin/org/example/aoc/year2024/day1/Day1.kt
@@ -0,0 +1,35 @@
+package org.example.aoc.year2024.day1
+
+import org.example.common.readFile
+import kotlin.math.absoluteValue
+
+fun main() {
+    val lines = readFile("\\year2024\\day1\\input.txt")
+    val day1 = Day1()
+    println(day1.part1(lines))
+    println(day1.part2(lines))
+}
+
+class Day1 {
+    fun part1(lines: List<String>): Int {
+        val (inputList1, inputList2) = parse(lines)
+        require(inputList1.size==inputList2.size)
+        val result = inputList1.sorted().zip(inputList2.sorted()) { a, b -> a.minus(b).absoluteValue }.sum()
+        return result
+    }
+
+    fun part2(lines: List<String>): Int {
+        val (inputList1, inputList2) = parse(lines)
+        val count = inputList2.groupingBy { it }.eachCount()
+        val result = inputList1.sumOf { count.getOrDefault(it, 0).times(it) }
+        return result
+    }
+
+    fun parse(lines: List<String>): Pair<List<Int>, List<Int>> {
+        return lines.fold(initial = Pair(emptyList<Int>(), emptyList<Int>())) { acc, line ->
+            val split = line.split("   ")
+            require(split.size == 2)
+            acc.first.plus(split[0].toInt()) to acc.second.plus(split[1].toInt())
+        }
+    }
+}
---
+++ src/test/kotlin/org/example/aoc/year2024/day1/Day1Test.kt
@@ -0,0 +1,21 @@
+package org.example.aoc.year2024.day1
+
+import org.example.common.readTestFile
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day1Test {
+
+    private val lines = readTestFile("\\year2024\\day1\\test_input.txt")
+    private val day1 = Day1()
+
+    @Test
+    fun part1Test() {
+        assertEquals(11, day1.part1(lines))
+    }
+
+    @Test
+    fun part2Test() {
+        assertEquals(31, day1.part2(lines))
+    }
+}
